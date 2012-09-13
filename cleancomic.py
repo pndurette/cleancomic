d2a80b516d320e62072db306d71df9926c373c1f
Pierre-Nick Durette <pndurette@gmail.com>
2012-09-13 02:25:33 -0400

    works, a lot of bugs to work out.. Needs a lots of cleanup

--- cleancomic.py
+++ cleancomic.py
@@ -1,6 +1,11 @@
 import os, sys, argparse, re
-import zipfile, tempfile, shutil
+import zipfile, zlib, tempfile, shutil
 import traceback
+
+import subprocess
+
+#from wand.image import Image
+#from wand.display import display
 
 # Process arguments
 def processArguments():
@@ -26,6 +31,7 @@ def processArguments():
 	args = parser.parse_args()
 	if not (args.contrast or args.trim or args.split):
 		parser.error('You need to specify at least one action. See --help')
+	args.destination = os.path.abspath(args.destination)
 	return args
 
 # Cleans one comic
@@ -36,27 +42,63 @@ def cleanOne(comic_path):
 		print "Error: %s skiped. Doesn't appear to be a zip file." % os.path.basename(comic_path)
 	else:
 		# 0. Init
+		(cbz_dir, cbz_name) = os.path.split(comic_path)
+		(cbz_basename, cbz_extension) = os.path.splitext(cbz_name)
 		cbz_work_dir = tempfile.mkdtemp()
-		cbz_new_dir = tempfile.mkdtemp()
+		os.mkdir("%s/%s" % (cbz_work_dir, cbz_basename))
+		cbz_work_dir = "%s/%s" % (cbz_work_dir, cbz_basename)
 		print "The cbz_work_dir: %s" % cbz_work_dir
-		print "The cbz_new_dir: %s" % cbz_new_dir
 		
 		# 1. Extract
-		cbz = zipfile.ZipFile(comic_path, 'r')
-		for f in cbz.namelist():
-			# If file match standard cbz file (<digits>.jpg) extract it
-			if re.match("^\d*.jpg$", f): 
-				cbz.extract(f, cbz_work_dir)
-		cbz.close()
+		# print comic_path
+		# cbz = zipfile.ZipFile(comic_path, 'r')
+		# for f in cbz.namelist():
+		# 	print "lala"
+		# 	# If file match standard cbz file (<digits>.jpg) extract it
+		# 	if re.match("^\d*.jpg$", f): 
+		# 		print "inside"
+		# 		cbz.extract(f, cbz_work_dir)
+		# cbz.close()
 		
-		# Do things
-		#if args.trim: actionTrim()
+		os.chdir(cbz_work_dir)
+		subprocess.call("unzip -j %s >/dev/null" % (comic_path), shell=True)
 		
-		# Re-compress to destination
+		# 2. Split
+		if args.split:
+			for f in os.listdir(cbz_work_dir):
+				subprocess.call("mogrify -format jpg -crop 50%%x100%% %s/%s" % (cbz_work_dir, f), shell=True)
+			
+			filecount = 0
+			for f in os.listdir(cbz_work_dir):
+				os.rename("%s/%s" % (cbz_work_dir, f),
+				          "%s/%03d.jpg" % (cbz_work_dir, filecount))
+				filecount+=1
+		
+		# 3. Trim
+		if args.trim:	
+			for f in os.listdir(cbz_work_dir):
+				fpath = "%s/%s" % (cbz_work_dir, f)
+				subprocess.call("convert %s -chop 5x5 -rotate 180 -chop 25x5 -rotate 180 %s" % (fpath,fpath), shell=True)
+				subprocess.call("convert %s -fuzz 40%% -trim +repage %s" % (fpath, fpath), shell=True)
+				
+		# 4. Boost contrast
+		# ...
+		
+		# 9. Recompress
+		# http://bip.weizmann.ac.il/course/python/PyMOTW/PyMOTW/docs/zipfile/index.html
+		#TODO import zipfile properly
+		# (dirName, fileName) = os.path.split(comic_path)
+		# 	new_cbz = zipfile.ZipFile('%s/%s' % (args.destination, fileName), 'w', zipfile.ZIP_DEFLATED)
+		# 	os.chdir(cbz_work_dir)
+		# 	for f in os.listdir(cbz_work_dir):
+		# 		new_cbz.write(f)
+		# 	new_cbz.close()
+		
+		os.chdir(cbz_work_dir)
+		subprocess.call("zip %s/%s *" % (args.destination, cbz_name), shell=True)
 		
 		# 10. Cleanup
 		shutil.rmtree(cbz_work_dir)
-		shutil.rmtree(cbz_new_dir)
 		
 		
 
